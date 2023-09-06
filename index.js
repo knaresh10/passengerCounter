8564c20b8293ab1a449878a585c7016cc1527ba2
knaresh10 <20P31A0594@acet.ac.in>
2023-09-06 07:05:58 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let countEl = document.getElementById("count-el")
-console.log(countEl)
 let count = 0
 function increment() {
     count = count + 1
     countEl.innerText = count
+}
+
+const saveEl = document.getElementById("save-el")
+
+function save() {
+    saveEl.textContent += count + " - "
+    countEl.textContent = 0
+    count = 0
 }
